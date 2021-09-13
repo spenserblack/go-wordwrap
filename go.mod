7701a2a6f1ae6285656c9619d46d14236ca84626
Spenser Black <spenserblack01@gmail.com>
2021-09-13 12:41:41 -0400

    Implement `WordWrap`

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/spenserblack/go-wordwrap
 
 go 1.16
+
+require github.com/mattn/go-runewidth v0.0.13 // indirect
--- wordwrap.go
+++ wordwrap.go
@@ -1,6 +1,12 @@
 // Package wordwrap is the main package.
 package wordwrap
 
+import (
+	"unicode"
+
+	"github.com/mattn/go-runewidth"
+)
+
 // WordWrap wraps a string, where each "line" of the wrapped string is an item
 // of a slice of strings. A limit of zero or less is treated as an infinite
 // limit.
@@ -12,5 +18,90 @@ package wordwrap
 // excluded from the output. Hyphens are also considered word separators, but
 // will be included at the end of a line.
 func WordWrap(s string, limit int) (lines []string) {
-	return []string{s}
+	if limit <= 0 {
+		return []string{s}
+	}
+
+	breakpoints := make([]breakpoint, 0)
+	var bp breakpoint
+	var currentLen int
+	reset := func() {
+		bp = nil
+		currentLen = 0
+	}
+	reset()
+
+	for i, char := range s {
+		if unicode.IsSpace(char) {
+			bp = spacepoint(i)
+		} else if char == '-' {
+			bp = hyphenpoint(i)
+		}
+		currentLen += runewidth.RuneWidth(char)
+
+		if currentLen >= limit {
+			if bp == nil {
+				bp = defaultpoint(i)
+			}
+			breakpoints = append(breakpoints, bp)
+			reset()
+		}
+	}
+
+	if len(breakpoints) == 0 {
+		return []string{s}
+	}
+
+	lines = append(lines, s[:breakpoints[0].Start()])
+	if len(breakpoints) == 1 {
+		lines = append(lines, s[breakpoints[0].End():])
+		return
+	}
+
+	for i, bp := range breakpoints[1:] {
+		prev := breakpoints[i]
+		lines = append(lines, s[prev.End():bp.Start()])
+	}
+
+	lines = append(lines, s[breakpoints[len(breakpoints)-1].End():])
+
+	return
+}
+
+// Breakpoint represents where a string should be broken to wrap.
+type breakpoint interface {
+	// Start is where the break should start.
+	Start() int
+	// End is where the break should end.
+	End() int
+}
+
+// Spacepoint is a breakpoint triggered by whitespace.
+type spacepoint int
+
+func (p spacepoint) Start() int {
+	return int(p)
+}
+func (p spacepoint) End() int {
+	return int(p) + 1
+}
+
+// Hyphenpoint is a breakpoint triggered by a hyphen.
+type hyphenpoint int
+
+func (p hyphenpoint) Start() int {
+	return int(p) + 1
+}
+func (p hyphenpoint) End() int {
+	return int(p) + 1
+}
+
+// Defaultpoint occurs when there is no available trigger for breakage.
+type defaultpoint int
+
+func (p defaultpoint) Start() int {
+	return int(p) + 1
+}
+func (p defaultpoint) End() int {
+	return int(p) + 1
 }
